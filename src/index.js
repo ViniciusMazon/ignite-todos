a813944a558141a9c0f8a8dd1a3467a728951774
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-08 21:43:07 -0300

    feat: implement create resource

--- src/index.js
+++ src/index.js
@@ -47,7 +47,19 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline + " 00:00"),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo);
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {

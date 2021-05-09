efc4c6c6233f99e3175b235d3634f8223c6ff8f1
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-08 21:27:05 -0300

    feat: implements middleware

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.header;
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(400).json({ error: 'User does not exist' });
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {

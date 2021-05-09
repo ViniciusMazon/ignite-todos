1b42b2a420cb28a632f2bb04d906bdf584959e5a
Vinicius Mazon <vnpmazon@gmail.com>
2021-05-08 21:31:25 -0300

    feat: implement todos index resource

--- src/index.js
+++ src/index.js
@@ -42,7 +42,8 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
